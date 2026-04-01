138e4c3e5755661a1cdecd79160a31db27a27dd5
Toru77 <berkaycelbis77@hotmail.com>
2026-04-01 16:43:40 +0300

    [Kai] Cleanup

--- src/games/kai-vanillaplus/xegtao_denoise_isfast_fp32.cs_5_0.hlsl
+++
@@ -1,2 +0,0 @@
-#define XE_GTAO_USE_HALF_FLOAT_PRECISION 0
-#include "./xegtao_denoise_isfast.cs_5_0.hlsl"
--- src/games/kai-vanillaplus/xegtao_main_fp32.cs_5_0.hlsl
+++
@@ -1,2 +0,0 @@
-#define XE_GTAO_USE_HALF_FLOAT_PRECISION 0
-#include "./xegtao_main.cs_5_0.hlsl"
